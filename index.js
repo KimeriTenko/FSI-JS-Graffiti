31766a263c2d7dc880a65d4ddd77db3501a7ef90
Mary-Frye <deimospho@gmail.com>
2022-04-05 12:51:06 -0400

    most of my changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let header = document.querySelector('#page-header')
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+let dogNames = document.querySelectorAll('dog-name')
+dogNames.style.textAlign = "left"
+let footer = document.querySelector('page-footer')
+footer.style.backgroundColor = "blue"
 
 // Select some elements...
